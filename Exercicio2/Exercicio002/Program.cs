54ed5e64e427db8ca941b2dcc9d92b833fcdd36f
Tiago Lopes <lopestiago127@gmail.com>
2024-03-07 13:49:41 -0300

    Primeiro commit

---
+++ Exercicio2/Exercicio002/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ExercicioDois
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> fibonacci = new List<int>();
+
+            Console.Write("Digite um número inteiro: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            int a = 0;
+            int b = 1;
+            int resultado = 0;
+            int i = 0;
+
+            while (resultado <= numero)
+            {
+                fibonacci.Add(a);
+
+                int aux = b;
+                b = a;
+                a = aux + b;
+
+                resultado = a;
+            }
+
+            Console.WriteLine();
+            if (fibonacci.Contains(numero)){
+                Console.WriteLine($"O número {numero} PERTENCE a sequência Fibonacci");
+            }
+            else
+            {
+                Console.WriteLine($"O número {numero} NÃO PERTENCE a sequência Fibonacci");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Exercicio5/Exercicio005/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ExercicioCinco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Escreva uma palavra: ");
+            string palavra = Console.ReadLine();
+            int tamanhoPalavra = palavra.Length - 1;
+            string palavraInvertida = "";
+
+            for (int i = tamanhoPalavra; i >= 0; i--)
+            {
+                palavraInvertida += palavra[i];
+            }
+
+            Console.WriteLine($"palavra invertida: {palavraInvertida}");
+        }
+    }
+}
